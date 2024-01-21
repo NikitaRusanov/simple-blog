a61b0964e76cf620831bcb4f1df2ba0a0822da5b
Nikita Rusanov <nipe322@gmail.com>
2024-01-21 13:12:50 +0300

    Change way to connect to database in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,39 @@
-import pytest
+import asyncio
 
-from src.database import database
+import pytest
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+from src.config import settings
 from src import Base
 
 
-@pytest.fixture(scope='session', autouse=True)
-async def setup_db():
-    async with database.engine.begin() as conenction:
+url = f'postgresql+asyncpg://{settings.db_user}:{settings.db_pass}@{settings.db_host}:{settings.db_port}/{settings.db_name}'
+
+@pytest.fixture(scope='session')
+def event_lopp():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope='session')
+def engine():
+    engine = create_async_engine(url)
+    print(url)
+    yield engine
+    engine.sync_engine.dispose()
+
+
+@pytest.fixture
+async def async_session(engine):
+    async with AsyncSession(engine) as session:
+        yield session
+
+
+@pytest.fixture(scope='function', autouse=True)
+async def setup_db(engine):
+    async with engine.begin() as conenction:
+        await conenction.run_sync(Base.metadata.drop_all)
+    yield
+    async with engine.begin() as conenction:
         await conenction.run_sync(Base.metadata.create_all)
