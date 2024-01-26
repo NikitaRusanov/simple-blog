c1262d0d467110a1f4cfc7f5ef6a13363c9af503
Nikita Rusanov <nipe322@gmail.com>
2024-01-26 11:30:02 +0300

    Add test for reading users and chane way to get connection to db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
@@ -7,27 +9,39 @@ from src.main import app
 from src.config import settings
 
 
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
 @pytest.fixture(scope='session')
 async def client():
-    async with AsyncClient(app=app, base_url='http://test') as async_client:
+    async with AsyncClient(app=app, base_url='http://test', follow_redirects=True) as async_client:
         yield async_client
 
 
-db_url = (f'postgresql+asyncpg://'
+@pytest.fixture(scope='session')
+def engine():
+    db_url = (f'postgresql+asyncpg://'
           f'{settings.db_user}:{settings.db_pass}@{settings.db_host}:{settings.db_port}/{settings.db_name}')
-engine = create_async_engine(db_url)
+    engine = create_async_engine(db_url, future=True)
+    yield engine
+    engine.sync_engine.dispose()
 
 
-@pytest.fixture(scope='session', autouse=True)
-async def create_tables():
+@pytest.fixture()
+async def create_tables(engine):
     async with engine.begin() as connection:
-        await connection.run_sync(Base.metadata.create_all)
-        yield
+        await connection.run_sync(Base.metadata.create_all)   
+    yield
+    async with engine.begin() as connection:
         await connection.run_sync(Base.metadata.drop_all)
 
 
-@pytest.fixture(scope='function')
-async def test_db():
-    async_session = AsyncSession(engine)
+@pytest.fixture(scope='session')
+async def test_db(engine):
+    async_session = AsyncSession(engine, expire_on_commit=False)
     yield async_session
     await async_session.close()
---
+++ tests/tets_users/conftest.py
@@ -0,0 +1,22 @@
+import asyncio
+
+import pytest
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src import User
+
+
+@pytest.fixture()
+async def user_test_samples(test_db: AsyncSession, create_tables):
+    samples = [
+        User(
+            email=f'test_email_{num}@mail.com',
+            username=f'test_user_{num}',
+            password=f'test_password_{num}'
+        )
+        for num in range(1, 4)
+    ]
+
+    test_db.add_all(samples)
+    await test_db.commit()
+    yield samples
