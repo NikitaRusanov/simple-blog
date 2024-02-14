9193a23d2c350e909072bfff998ee4d2e6ac7d33
Nikita Rusanov <nipe322@gmail.com>
2024-02-14 08:57:51 +0300

    Add function for getting user from jwt

--- src/auth/router.py
+++ src/auth/router.py
@@ -3,10 +3,13 @@ from fastapi.security import OAuth2PasswordBearer
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from jwt.exceptions import InvalidTokenError
+
 import user.models as user_models
 import auth.service as auth_service
 import database
 import auth.utils as auth_utils
+import user.service as user_servie
 
 
 router = APIRouter(tags=["Auth"])
@@ -35,7 +38,23 @@ async def login(user: user_models.User = Depends(validate_user)):
         "email": user.email,
     }
     token = auth_utils.encode_token(token_payload)
-    return {
-        "token": token,
-        "token_type": "Bearer"
-    }
+    return {"access_token": token, "token_type": "Bearer"}
+
+
+async def get_user_from_token(
+    token: str = Depends(oauth2_scheme),
+    session: AsyncSession = Depends(database.database.scoped_session_dependency),
+) -> user_models.User | None:
+
+    auth_err = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+    )
+    try:
+        payload = auth_utils.decode_token(token)
+    except InvalidTokenError as e:
+        raise auth_err
+
+    user_id = int(payload.get("sub"))  # type: ignore
+    if not (user := await user_servie.get_user(session, user_id)):
+        raise auth_err
+    return user
