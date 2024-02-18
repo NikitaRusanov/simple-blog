df7a4934cb8947d342f52d5db4679e9bd5adf350
Nickita Rusanov <nipe322@gmail.com>
2024-02-18 08:39:48 +0300

    Add auth headers for updating tests

--- tests/test_users/test_get_users.py
+++ tests/test_users/test_get_users.py
@@ -48,9 +48,9 @@ async def test_get_one_user(
     assert resp_data.get("email") == target_user.email
 
 
-async def test_ueser_not_found(client: AsyncClient, create_tables):
-    test_user_id = 1
-    resp = await client.get(f"/user/{test_user_id}")
+async def test_ueser_not_found(client: AsyncClient, create_tables, test_auth_headers):
+    test_user_id = 123
+    resp = await client.get(f"/user/{test_user_id}", headers=test_auth_headers)
 
     assert resp.status_code == 404
     assert resp.json().get("detail") == f"User {test_user_id} not found"
--- tests/test_users/test_update_users.py
+++ tests/test_users/test_update_users.py
@@ -19,13 +19,13 @@ from httpx import AsyncClient
         ({}),
     ],
 )
-async def test_update_user(test_data, client: AsyncClient, user_test_samples, get_user):
+async def test_update_user(test_data, client: AsyncClient, user_test_samples, get_user, test_auth_headers):
     test_user = user_test_samples[0].__dict__.copy()
     test_user.pop("_sa_instance_state")
     test_user_id = test_user.get("id")
     test_user.update(test_data)
 
-    resp = await client.patch(f"/user/{test_user_id}", json=test_data)
+    resp = await client.patch(f"/user/{test_user_id}", json=test_data, headers=test_auth_headers)
     assert resp.status_code == 200
 
     db_user = await get_user(test_user_id)
@@ -35,17 +35,17 @@ async def test_update_user(test_data, client: AsyncClient, user_test_samples, ge
     assert db_user.password == test_user["password"]
 
 
-async def test_user_not_found(client: AsyncClient, create_tables):
-    test_user_id = 1
-    resp = await client.patch(f"/user/{test_user_id}")
+async def test_user_not_found(client: AsyncClient, create_tables, test_auth_headers):
+    test_user_id = 123
+    resp = await client.patch(f"/user/{test_user_id}", headers=test_auth_headers)
 
     assert resp.status_code == 404
     assert resp.json().get("detail") == f"User {test_user_id} not found"
 
 
-async def test_no_body_create(client: AsyncClient, user_test_samples):
+async def test_no_body_create(client: AsyncClient, user_test_samples, test_auth_headers):
     test_user_id = 1
-    resp = await client.patch(f"/user/{test_user_id}")
+    resp = await client.patch(f"/user/{test_user_id}", headers=test_auth_headers)
 
     assert resp.status_code == 422
     assert resp.json().get("detail")[0]["type"] == "missing"
