5dcad4aabf08024cadbeb61a83159cd4f1fda3d8
Nickita Rusanov <nipe322@gmail.com>
2024-02-18 08:31:41 +0300

    Add auth headers to deleting tests and add src volume to test DC config

--- tests/test_users/test_delete_users.py
+++ tests/test_users/test_delete_users.py
@@ -1,9 +1,11 @@
 from httpx import AsyncClient
 
 
-async def test_delete_user(client: AsyncClient, user_test_samples, get_user):
+async def test_delete_user(
+    client: AsyncClient, user_test_samples, get_user, test_auth_headers
+):
     test_user_id = 1
-    resp = await client.delete(f"/user/{test_user_id}")
+    resp = await client.delete(f"/user/{test_user_id}", headers=test_auth_headers)
 
     assert resp.status_code == 200
 
@@ -11,9 +13,11 @@ async def test_delete_user(client: AsyncClient, user_test_samples, get_user):
     assert db_user is None
 
 
-async def test_delete_not_found_user(client: AsyncClient, create_tables):
-    test_user_id = 1
-    resp = await client.delete(f"/user/{test_user_id}")
+async def test_delete_not_found_user(
+    client: AsyncClient, create_tables, test_auth_headers
+):
+    test_user_id = 123
+    resp = await client.delete(f"/user/{test_user_id}", headers=test_auth_headers)
 
     assert resp.status_code == 404
     assert resp.json().get("detail") == f"User {test_user_id} not found"
